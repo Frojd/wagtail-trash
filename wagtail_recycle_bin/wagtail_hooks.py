ca65532f8a7fc18e86d5825a88e0860ce89882a9
Andreas Bernacca <hello@rinti.se>
2020-10-14 08:49:08 +0200

    Add stub restore button

--- wagtail_recycle_bin/wagtail_hooks.py
+++ wagtail_recycle_bin/wagtail_hooks.py
@@ -3,10 +3,37 @@ from wagtail.core import hooks
 from wagtail.core.models import Page
 from wagtail.contrib.modeladmin.options import ModelAdmin, modeladmin_register
 from wagtail.contrib.modeladmin.views import IndexView
+from wagtail.contrib.modeladmin.helpers import ButtonHelper
 
 from .utils import recycle_bin_for_request
 from .models import RecycleBinPage
 from .views import recycle_delete
+
+
+class RecycleButtonHelper(ButtonHelper):
+    restore_button_classnames = ["button-small", "button-secondary", "icon", "icon-undo"]
+
+    def restore_button(self, obj):
+        return {
+            "url": "/",
+            "label": "Restore",
+            "classname": self.finalise_classname(self.restore_button_classnames),
+            "title": "Restore",
+        }
+
+    def get_buttons_for_obj(
+        self, obj, exclude=["edit"], classnames_add=None, classnames_exclude=None
+    ):
+        buttons = super().get_buttons_for_obj(
+            obj, exclude, classnames_add, classnames_exclude
+        )
+
+        print(buttons)
+
+        if "restore" not in (exclude or []):
+            buttons.append(self.restore_button(obj))
+
+        return buttons
 
 
 class RecycleBinModelAdmin(ModelAdmin):
@@ -17,6 +44,8 @@ class RecycleBinModelAdmin(ModelAdmin):
 
     list_display = ("title", "sub_pages")
     search_fields = ("title",)
+
+    button_helper_class = RecycleButtonHelper
 
     def sub_pages(self, page):
         return [x.title for x in page.get_descendants()]
