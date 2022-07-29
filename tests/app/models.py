d12679ac60338bd20db015de6b3d790b016112a4
Andreas Bernacca <hello@rinti.se>
2022-07-29 20:23:05 +0200

    Add support for Wagtail 3.0

--- tests/app/models.py
+++ tests/app/models.py
@@ -1,11 +1,13 @@
-from wagtail.core.models import Page
+from wagtail.core.models import Page, PageManager
 
 from wagtail_trash.managers import TrashManager
 
 
 class TestPage(Page):
+    objects = PageManager()
     objects_excluding_bins = TrashManager()
 
 
 class OtherPage(Page):
+    objects = PageManager()
     objects_excluding_bins = TrashManager()
